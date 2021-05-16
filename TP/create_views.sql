1322e9e3a235d74a65e562d826adc59ede2c4ec3
BakaDobe <59211321+BakaDobe@users.noreply.github.com>
2021-05-15 22:55:09 -0400

    Update create_views.sql

--- TP/create_views.sql
+++ TP/create_views.sql
@@ -18,3 +18,7 @@ CREATE OR REPLACE VIEW VW_AIRPLANE_INFO AS
 	SELECT a.airline_id, a.al_company, ap.a_capacity, ap.plane_id, fl.f_route, fl.f_terminalnum
 	FROM AIRPLANE ap, AIRLINE a, FLIGHT fl 
 	WHERE a.airline_id = ap.airline_id AND a.airline_id = fl.airplane_id WITH READ ONLY;
+	
+	
+DROP VIEW VW_TRAVELER_INFO;
+DROP VIEW VW_AIRPLANE_INFO;
